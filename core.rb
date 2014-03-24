d8c98ccffedebe80b984d825ea2bc08beaad3d4c
c <freqnlabs@gmail.com>
2014-03-24 00:20:29 -0400

    Refactored dungeon class

--- core.rb
+++ core.rb
@@ -6,22 +6,9 @@ class Dungeon
 		@rooms = []
 	end
 
-	class Player
-		attr_accessor :name, :location
-
-		def initialize(player_name)
-			@name = player_name
-		end
-	end
-
-	class Room
-		attr_accessor :reference, :name, :description, :connections
-
-		def initialize(reference, name, description, connections)
-			@reference = reference
-			@name = name
-			@description = description
-			@connections = connections
-		end
-	end
+	Player = Struct.new(:name, :location)
+	Room = Struct.new(:reference, :name, :description, :connections)
 end
+
+#my_dungeon = Dungeon.new("Tom Spirk")
+#puts my_dungeon.player.name
