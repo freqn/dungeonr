c8de82fccad6c22ae244e82cd23b6430387d6dd2
c <freqnlabs@gmail.com>
2014-03-25 08:17:11 -0400

    Added location detection

--- core.rb
+++ core.rb
@@ -7,18 +7,65 @@ class Dungeon
 	end
 
 	def add_room(reference, name, description, connections)
-		@room << Room.new(reference, name, description, connections)
+		@rooms << Room.new(reference, name, description, connections)
 	end
 
-	Player = Struct.new(:name, :location)
-	Room = Struct.new(:reference, :name, :description, :connections)
+	def start(location)
+		@player.location = location
+		show_current_description
+	end
 
+	def show_current_description
+		puts find_room_in_dungeon(@player.location).full_description
+	end
 
+	def find_room_in_dungeon(reference)
+		@rooms.detect {|room| room.reference == reference}
+	end
+
+	def find_room_in_direction(direction)
+		find_room_in_dungeon(@player.location).connections[direction]
+	end
+
+	def go(direction)
+		puts "You go " + direction.to_s
+		@player.location = find_room_in_direction(direction)
+		show show_current_description
+	end
+
+	class Player
+		attr_accessor :name, :location
+
+		def initialize(name)
+			@name = name
+		end
+	end
+
+	class Room
+		attr_accessor :reference, :name, :description, :connections
+
+		def initialize(reference, name, description, connections)
+			@reference = reference
+			@name = name
+			@description = description
+			@connections = connections
+		end
+
+		def full_description
+			@name + "\n\nYou are in " + @description
+		end
+	end
 end
 
-x = Dungeon.new("Tom Spirk")
-puts x.player.name
+# Create the main dungeon object
+my_dungeon = Dungeon.new("Tom Spirk")
+puts my_dungeon.player.name
 
-x.add_room(:largecave, "Large Cave", "a large cavernous cave", {:west => :smallcave})
-x.add_room(:smallcave, "Small Cave", "a small, claustrophobic cave", {:east => :largecave})
+# Add rooms to the dungeon
+my_dungeon.add_room(:largecave, "Large Cave", "a large cavernous cave", {:west => :smallcave, :south => :warroom})
+my_dungeon.add_room(:smallcave, "Small Cave", "a small, claustrophobic cave", {:east => :largecave, :south => :foodcave})
+my_dungeon.add_room(:foodcave, "Food cave", "a food storage area", {:north => :smallcave, :east => :warroom})
+my_dungeon.add_room(:warroom, "War Room", "a strategy room with an arsenal", {:west => :foodcave, :north => :largecave})
 
+# Start the dungeon by placing the player in a large cave
+my_dungeon.start(:largecave)
